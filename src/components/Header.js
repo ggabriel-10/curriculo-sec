56bc20c6848915ab8c7735c907df9343d1e6cf76
Giovanni Medeiros <ggabriel.medeiros@hotmail.com>
2023-07-12 18:01:46 -0300

    Alteração nomeclatura

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ export function Header() {
                     <nav className='navegation'>
                         <img src="https://cdn.icon-icons.com/icons2/2072/PNG/512/antivirus_internet_protect_safe_security_shape_shield_icon_127103.png" className="App-logo" alt="logo" />
                         <ul>
-                            <li><a className='item-navegation' href="#header">Home</a></li>
+                            <li translate="no"><a className='item-navegation' href="#header">Home</a></li>
                             <li><a className='item-navegation' href="#conteudo">About Me</a></li>
                             <li><a className='item-navegation' href="#study">Courses</a></li>
                             <li><a className='item-navegation' href="#certificados">Certificates</a></li>
@@ -24,7 +24,7 @@ export function Header() {
                         <FaBars className='item-bars' onClick={addClass}/>
                         <ul className='navegation-mobile-item' id='nav-mobile'>
                             <IoClose size={45} className='item-close' onClick={removeClass}/>
-                            <li><a className='item-navegation' href="#header"onClick={removeClass}>Home</a></li>
+                            <li translate="no"><a className='item-navegation' href="#header"onClick={removeClass}>Home</a></li>
                             <li><a className='item-navegation' href="#conteudo"onClick={removeClass}>About Me</a></li>
                             <li><a className='item-navegation' href="#study"onClick={removeClass}>Courses</a></li>
                             <li><a className='item-navegation' href="#certificados"onClick={removeClass}>Certificates</a></li>
